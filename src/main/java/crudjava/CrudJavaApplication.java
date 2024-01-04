dd7dd9d6dde012abc435baa107eadadd4485a793
Jason C <grousemusic@gmail.com>
2024-01-04 14:28:51 -0500

    Initial commit

---
+++ src/main/java/crudjava/CrudJavaApplication.java
@@ -0,0 +1,13 @@
+package crudjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crudjava/CrudJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package crudjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
